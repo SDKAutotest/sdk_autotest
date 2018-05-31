9a95b3175ccdbf005670650e6f12a8f2b6fa39b3
wangzhikun <wangzhikun.01@bytedance.com>
2018-05-31 21:23:13 +0800

    elements类，Android在初始化时获取首页元素

--- elements.py
+++ elements.py
@@ -15,7 +15,12 @@ class android():
 
 	def __init__(self,driver):
 		self.driver = driver
+		self.home_elements =self.home_element()
 		print("android devices elements.....")
+
+	def home_element(self):
+		print("初始化获取元素的方法时，直接获取到整个首页的元素列表")
+		return 1
 
 	def splash_elements(self):
 
